af1733608e46699ccdf271f583939f4b6c749b5c
Juan Camilo Ibáñez <juancamilo0414@hotmail.es>
2023-06-29 20:31:13 -0500

    Section 2 - Chapter 40

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,13 +1,45 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserInput(){
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCal, calNumber) {
+    const calcDescription = `${resultBeforeCal} ${operator} ${calNumber}`;
+    outputResult(currentResult, calcDescription)
+}
 
 function add(num1, num2) {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber; // numA + + stringB == parseInt(stringB)
+    createAndWriteOutput('+',initialResult,enteredNumber)
 
 }
 
+function subtract() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-',initialResult,enteredNumber)
+}
+function multiply() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*',initialResult,enteredNumber)
+}
+function divide() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/',initialResult,enteredNumber)
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
