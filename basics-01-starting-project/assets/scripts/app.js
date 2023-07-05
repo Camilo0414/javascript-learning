4725e14380cdc1b17561893d017f627694d0eccd
Juan Camilo Ibáñez <juancamilo0414@hotmail.es>
2023-07-04 20:11:40 -0500

    Section 2 - Chapter 46

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,8 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
+
+// Comments :)
 
 function getUserInput(){
     return parseInt(userInput.value);
@@ -10,31 +13,40 @@ function createAndWriteOutput(operator, resultBeforeCal, calNumber) {
     outputResult(currentResult, calcDescription)
 }
 
-function add(num1, num2) {
+function add() {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber; // numA + + stringB == parseInt(stringB)
-    createAndWriteOutput('+',initialResult,enteredNumber)
-
+    currentResult += enteredNumber; // numA + + stringB == parseInt(stringB)
+    createAndWriteOutput('+',initialResult,enteredNumber);
+    const logEntry = {
+        operation: 'ADD',
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
 }
 
 function subtract() {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
-    createAndWriteOutput('-',initialResult,enteredNumber)
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-',initialResult,enteredNumber);
 }
+
 function multiply() {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
-    createAndWriteOutput('*',initialResult,enteredNumber)
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*',initialResult,enteredNumber);
 }
+
 function divide() {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
-    createAndWriteOutput('/',initialResult,enteredNumber)
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/',initialResult,enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
